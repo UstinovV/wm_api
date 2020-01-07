3118daa9b15548478419b3c5bf8666124e0b7419
ustinov.vd <ustinov.vd@gmail.com>
2020-01-07 18:24:37 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	_"log"
+	_"net/http"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("API")
+	//http.HandleFunc("/api/index", indexHandler)
+	//http.HandleFunc("/api/repo/", repoHandler)
+	//log.Fatal(http.ListenAndServe("localhost:8000", nil))
+
+}

cdbc63b8d6ab2e7026374d3d09eebeca785c2c67
ustinov.vd <ustinov.vd@gmail.com>
2020-01-10 18:43:34 +0100

    DB connect

--- main.go
+++ main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
-	_"log"
-	_"net/http"
+	_ "github.com/lib/pq"
+	"database/sql"
 	"fmt"
+)
+
+const (
+	host     = "172.20.0.3"
+	port     = 5432
+	user     = "postgres"
+	password = ""
+	dbname   = "wm"
 )
 
 func main() {
 	fmt.Println("API")
-	//http.HandleFunc("/api/index", indexHandler)
-	//http.HandleFunc("/api/repo/", repoHandler)
-	//log.Fatal(http.ListenAndServe("localhost:8000", nil))
-
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 }
